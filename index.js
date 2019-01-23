5d3820c566784e4b172fae107f44177d1db9d793
Ayana Bando <ayanaflax@yahoo.com>
2019-01-23 21:21:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 $(document).ready(function (){
 });
-function searchRepositories(){
-	const nameValue = document.getElementById("searchTerms").value;
-	// req.addEventListener('load', displayRepositories);
-	const url = `https://api.github.com/users/${nameValue}/repos`
-	$.get(url, function(data) {
-		console.log(data)
-		// displayRepositories();
-	}).fail(function(error) {
-		console.log(error)
-		//error
-	});
-	// req.open('GET', `https://api.github.com/users/${nameValue}/repos`);
-	// req.send();
+ $('#link').click(function(){
+    searchRepositories();
+  })
+});
+
+function searchRepositories() {
+  let searchTerms = document.getElementById("searchTerms").value
+  let URI = 'https://api.github.com/search/repositories?q=' + searchTerms;
+
+  $.get(URI, function(response){
+    let info = response.items.map(r => 
+      `<h3>Repo: <a href="${r.html_url}">${r.name}</a></h3>
+      <img src="${r.owner.avatar_url}" width="100" height="60"/>
+      <li>Owner: ${r.owner.login}</li>
+      <li><a href="${r.owner.html_url}">Profile</a></li>
+      <li>Description: ${r.description}</li>
+      <li><a href="#" data-owner=${r.owner.login} data-repository=${r.name} onclick="showCommits(this)">show commits</a><br>`
+    ).join(' ')
+    $('#results').html(info)
+  }).fail(function(){
+    displayError();
+  })
 }
 
-function displayRepositories(){
-	// get the data and display on the website
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
-// var url =
-//     'https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078';
- 
-// $.get(url).done(function(data) {
-//     console.log('Done');
-//     console.log(data);
-// });
+function showCommits(el) {
+  let URI = "https://api.github.com/repos/" + el.dataset.owner + "/" + el.dataset.repository + "/commits"
+
+  $.get(URI, function(response){
+    console.log(response)
+    let info = response.map(r => `
+    <li>SHA: ${r.sha}</li>
+    <li>Author: ${r.commit.author.name}</li>
+    <li>Login: ${r.committer.login}</li><br>`).join(' ')
+
+    $('#details').html(info)
+  }).fail(function(){
+    displayError();
+  })
+}
 
 // $.get('this_doesnt_exist.html', function(data) {
 //     doSomethingGood();
